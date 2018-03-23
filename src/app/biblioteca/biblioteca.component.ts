7e06588694bf16d555440f934518a7b95c27490a
Ana Paula <ana@gendesigns.com.br>
2018-03-23 13:52:46 -0300

    Arrumando tamanho dos botoes da biblioteca

--- src/app/biblioteca/biblioteca.component.ts
+++ src/app/biblioteca/biblioteca.component.ts
@@ -38,7 +38,15 @@ export class BibliotecaComponent implements OnInit {
         fullscreen: '<i class="fas fa-arrows-alt"></i>',
         borderless: '<i class="fas fa-expand"></i>',
         close: '<i class="fas fa-times"></i>'
-    },
+      },
+      previewZoomButtonClasses: {
+        prev: 'btn btn-navigate',
+        next: 'btn btn-navigate',
+        toggleheader: 'btn btn-tiny btn-kv btn-default btn-outline-secondary',
+        fullscreen: 'btn btn-tiny btn-kv btn-default btn-outline-secondary',
+        borderless: 'btn btn-tiny btn-kv btn-default btn-outline-secondary',
+        close: 'btn btn-tiny btn-kv btn-default btn-outline-secondary'
+      }
     });
 
     $("#input-id").on('filepreupload', function (event, data, previewId, index) {
